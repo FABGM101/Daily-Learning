2481287f30b55ede945b672f237aae649cd9d60a
babbayaga <flavioabgmiguel@gmail.com>
2026-02-22 18:43:25 -0300

    Added tabulate and better visualization to the user side.

--- Python-Basic/Stock-Manager.py
+++ Python-Basic/Stock-Manager.py
@@ -2,17 +2,21 @@
 # Instruções: Use um laço while True para pedir ao usuário que insira o nome de um item e a quantidade.
 # Se ele digitar "sair", use o comando break.
 # Armazene os itens em um Dicionário, usando o nome como chave e a quantidade como valor
+from tabulate import tabulate as tb
+RED = "\033[31m"
+BOLD = "\033[1m"
+RESET = "\033[0m"
 
 stock = {}
 
 while True:
-    product = input("Type the product (or 'exit' to exit the app):  ")
+    product = input(f"{BOLD}Type the product{RESET} (or 'exit' to exit the app){BOLD}:{RESET}  ")
     if product.lower() == 'exit':
         break
     try:
         qtt = int(input(f"Type the {product} quantity:  "))
         stock[product] = qtt
     except ValueError:
-        print("Error: please type a number for the quantity")
+        print(f"{RED}Error{RESET}: please type a number for the quantity")
 
-print("Final stock: ", stock)
+print(tb(stock.items(), headers=["Prod.", "Amount"]))
