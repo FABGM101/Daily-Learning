68b3cefd12b35e3a864ab307d2e92b080067b379
babbayaga <flavioabgmiguel@gmail.com>
2026-03-05 22:46:20 -0300

    Added a wax seal, and data gathering.

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,76 @@
 const formulario = document.querySelector('form');
 const elementoMensagem = document.getElementById('welcome');
+const selo = document.querySelector('.seal');
+const selomsg = document.querySelector('.seal-msg');
+const seloMensagem = document.getElementById('seal-msg');
+const heroiSalvo = localStorage.getItem('heroiReg');
+const emailSalvo = localStorage.getItem('emailReg');
+const logout = document.getElementById('btn-logout');
+
+if(heroiSalvo && emailSalvo){
+    formulario.classList.add('vanish');
+    logout.style.display = 'flex';
+    logout.addEventListener('click', function(){
+        localStorage.removeItem('heroiReg');
+        localStorage.removeItem('emailReg');
+        location.reload();
+    });
+    ShowSeal();
+    SealMsgWelcomeBack();
+}
+
+function ShowSeal() {
+selo.style.display = 'block';
+selo.style.top = '50%';
+selo.style.left = '50%';
+selo.style.transform = 'translate(-50%, -50%)';
+}
+function SealMsg(){
+    seloMensagem.style.display = 'flex';
+    seloMensagem.innerHTML = `A Guilda da Folha o espera!`;
+}
+function SealMsgWelcomeBack(){
+    seloMensagem.style.display = 'flex';
+    seloMensagem.innerHTML = `Bem vindo de volta, ${heroiSalvo}`;
+}
+
 formulario.addEventListener('submit', function(evento){
     evento.preventDefault()
     const campoNome = document.getElementById('name');
     const campoEmail = document.getElementById('email');
-    elementoMensagem.innerHTML = `Bem vindo a guilda, ${campoNome.value}! <br>
+    if(campoNome.value === ""){
+        elementoMensagem.classList.remove('success-msg');
+        elementoMensagem.classList.add('error-msg');
+        elementoMensagem.innerHTML = `Opa Opa Opa, aonde você pensa que vai sem um nome? GUARDAS!`
+        setTimeout(function(){
+            elementoMensagem.innerHTML = ``;
+            elementoMensagem.classList.remove('error-msg');
+        }, 3000);
+    }else if (campoEmail.value === ""){
+        elementoMensagem.classList.remove('success-msg');
+        elementoMensagem.classList.add('error-msg');
+        elementoMensagem.innerHTML = `Ei! ${campoNome.value}! Sem um endereço, não temos para onde mandar as coru- <br>
+                                        Larguem elas! Malditos pivetes...`
+    }else{
+        elementoMensagem.classList.remove('error-msg');
+        elementoMensagem.classList.add('success-msg');
+        elementoMensagem.innerHTML = `Bem vindo a guilda, ${campoNome.value}! <br>
                                     Enviaremos uma coruja para ${campoEmail.value},
                                     assim que essas crianças infelizes pararem de jogar pedras nelas... <br>
-                                    Ah! Cuidado, há goblins por perto...`;
+                                    Ah! Cuidado, há goblins por perto...`
+        formulario.classList.add('vanish');
+        localStorage.setItem('heroiReg', campoNome.value);
+        localStorage.setItem('emailReg', campoEmail.value);
+        setTimeout(function(){
+            elementoMensagem.innerHTML = ``;
+            ShowSeal();
+            SealMsg();
+        },3000);
+    }
 });
 formulario.addEventListener('reset', function(evento){
-    evento.preventDefault()
-    alert("Ora Ora Ora... Se não é um maldito goblin na minha guilda. SAIA!");
+    elementoMensagem.classList.remove('success-msg');
+    elementoMensagem.classList.add('error-msg');
+    elementoMensagem.innerHTML = 'Ora Ora Ora... Se não é um maldito goblin na minha guilda. SAIA!'
+
 });
