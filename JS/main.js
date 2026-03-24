a2d85777eab17ee66dbd7a91a3368b7c33d391fe
babbayaga <flavioabgmiguel@gmail.com>
2026-03-24 18:08:39 -0300

    Added a 'Add mission' option to the taskboard.

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,24 @@
 const formulario = document.querySelector('form');
 const elementoMensagem = document.getElementById('welcome');
 
+const NewMission = document.getElementById('new-mission');
+const NewMissionBtn = document.getElementById('new-mission-btn');
+const NewMissionClass = document.querySelector('.newmission');
+const NewMissionBtnClass = document.querySelector('.newmission-btn');
+
+
 const selobtn = document.getElementById('btn-enter');
 const selo = document.querySelector('.seal');
 const selomsg = document.querySelector('.seal-msg');
 const seloMensagem = document.getElementById('seal-msg');
+
 const heroiSalvo = localStorage.getItem('heroiReg');
 const emailSalvo = localStorage.getItem('emailReg');
+
 const logout = document.getElementById('btn-logout');
 const BodyBackground = document.getElementById('body-a');
 const MuralArea = document.getElementById('mural');
+
 const TaskMural = [
     "Derrote os goblins na caverna ao norte de Voidsky",
     "Ajude o rei de Kylmane com os protestos em prol dos não-humanos",
@@ -36,7 +45,13 @@ selobtn.addEventListener('click', function(){
     MuralArea.classList.add('Mural');
     BodyBackground.classList.add('body-transition'); 
     
-    MuralArea.innerHTML = "<h3>Mural de missões disponíveis:</h3>";
+    const MissionMural = document.createElement('h3');
+    MissionMural.textContent = 'Mural de missões';
+    MuralArea.prepend(MissionMural);
+
+    const MissionZone = document.getElementById('mission-zone');
+
+
     TaskMural.forEach(function(Task){
         const NewParagraph = document.createElement('p');
         
@@ -65,7 +80,7 @@ selobtn.addEventListener('click', function(){
             }
         });
 
-        MuralArea.appendChild(NewParagraph);
+        MissionZone.appendChild(NewParagraph);
     });
 
     const MissionList = document.querySelectorAll('.Missions');
@@ -134,5 +149,57 @@ formulario.addEventListener('reset', function(evento){
     elementoMensagem.classList.remove('success-msg');
     elementoMensagem.classList.add('error-msg');
     elementoMensagem.innerHTML = 'Ora Ora Ora... Se não é um maldito goblin na minha guilda. SAIA!'
+
+});
+
+NewMissionClass.addEventListener('submit', function(evento){
+    evento.preventDefault();
+
+    const textMission = NewMission.value;
+
+
+    if(textMission.trim() === "") return;
+
+    const NewParagraph = document.createElement('p');
+
+    NewParagraph.textContent = `🪶${textMission}`;
+    NewParagraph.classList.add('Missions');
+    NewParagraph.dataset.text = `🪶${textMission}`;
+
+    NewParagraph.addEventListener('click', function(){
+
+        if(NewParagraph.classList.contains('mission-acquired')){
+
+            NewParagraph.classList.add('revert-acquired');
+
+            setTimeout(function(){
+                NewParagraph.textContent = `🪶${textMission}`;
+                NewParagraph.classList.remove('revert-acquired');
+                NewParagraph.classList.remove('mission-acquired');
+                    
+                NewParagraph.classList.add('Missions');
+            }, 400);
+        }
+        else{
+            NewParagraph.textContent = `${textMission}`;
+            NewParagraph.classList.remove('Missions');
+            NewParagraph.classList.add('mission-acquired');
+        }
+    });
+
+    NewParagraph.addEventListener('mousemove', (evento) => {
+        const retangulo = NewParagraph.getBoundingClientRect();
+        const x = evento.clientX - retangulo.left;
+        const y = evento.clientY - retangulo.top;
+
+        NewParagraph.style.setProperty('--x', `${x}px`);
+        NewParagraph.style.setProperty('--y', `${y}px`);
+    });    
+
+        
+    const MissionZone = document.getElementById('mission-zone');
+    MissionZone.appendChild(NewParagraph);
+    NewMission.value = '';
+
 
 });
